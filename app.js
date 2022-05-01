3ba74283bf44355e0e47c70e0fb00a39bdb4a2f7
MaxDeJune <91304067+MaxDeJune@users.noreply.github.com>
2022-05-01 11:32:02 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+import {useState} from 'react'; 
+
+export default function App() {
+
+  const [post, setPost] = useState([{id: 1, title: 'Название поста', body: 'Сам пост здесь'}]);
+  
+  const [newPost, setNewPost] = useState({title: '', body: ''});
+  
+  const addPost =(e) => {
+    e.preventDefault(); 
+    setPost([...post, {...newPost, id: Date.now()}]); 
+    setNewPost({title:'', body:''}); 
+  }
+  
+  return (
+    <div className="App">
+      <form>
+        <input value={newPost.title} onChange={e => setNewPost({...newPost, title: e.target.value})}/> 
+        <input value={newPost.body} onChange={e => setNewPost({...newPost, body: e.target.value})}/>
+        <button onClick={addPost}>Добавить пост</button>
+      </form>
+      {post.map((e, index) => <div key={newPost.id}>{index + 1} | {e.title} | {e.body}</div>)}
+    </div>
+  );
+}
